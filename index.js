dc768a07bd6f6a79f7b837d1409d437ec9456849
+ <+>
2024-01-16 17:04:12 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const divide = function () {
+    2000/100
+ return 2000/100;
+}
+divide ();
+
+const square = (num) => num * num;
+square(3);
+
+const add = (parameter1 ,parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;}
+add();
+
+
